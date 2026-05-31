bab845cae0a37b739115de58ad6976f4e723965f
Кадрян Данила <danya-7779@mail.com>
2026-05-31 21:29:25 +0300

    первая правка

--- fit_life.py
+++ fit_life.py
@@ -1,24 +1,30 @@
 # вводим константы
 WATER_PER_KG = 30
 MILLILITRES_PER_LITRES = 1000
+
 # собираем данные
-print ("Здравствуйте! Укажите ваше имя:")
+print("Здравствуйте! Укажите ваше имя:")
 user_name = input ()
-print ("Приятно познакомиться,", user_name.title(),"!")
-print ("Укажите ваш возраст:")
+print("Приятно познакомиться,", user_name.title(),"!")
+print("Укажите ваш возраст:")
 user_age = int(input ())
-print ("Укажите ваш вес в килограммах (например, 70.5):")
+print("Укажите ваш вес в килограммах (например, 70.5):")
 user_weight = float(input())
-print ("Укажите ваш рост в метрах (например, 1.55):")
+print("Укажите ваш рост в метрах (например, 1.55):")
 user_height = float(input())
+
 # считаем индекс массы тела
 bmi = user_weight / (user_height ** 2)
+
 # считаем норму воды
 water_ml = user_weight * WATER_PER_KG
+
 # переводим норму воды в литры
 water_l = water_ml / MILLILITRES_PER_LITRES
+
 # печатаем отчет
-print (f"Отчет для пользователя: {user_name} {user_age} (года/лет)")
-print (f"Твой Индекс Массы Тела: {round(bmi, 1)}")
-print (f"Рекомендуемая норма воды: {water_l} л. в день")
-print ("Расчет окончен. Будьте здоровы!")
+print(f"Отчет для пользователя: {user_name.title()} {user_age} (года/лет)")
+print(f"Твой Индекс Массы Тела: {bmi:.1f}")
+print(f"Рекомендуемая норма воды: {water_l:.1f} л. в день") 
+print("Расчет окончен. Будьте здоровы!")
+
